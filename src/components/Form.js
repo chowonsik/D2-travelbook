5bbd2feb9c35b31785723cc0c488f1a1aa60e5fe
jhktm <jhktm1729@naver.com>
2019-07-23 15:39:43 +0900

    debug solved

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, AsyncStorage, Keyboard } from 'react-native';
 import {Actions} from 'react-native-router-flux';
-import {firebase_login, firebase_register} from 'c:/Users/user/react-native/login/reducer/App_reducer';
+import {firebase_login, firebase_register} from '../../reducer/App_reducer';
 
 export default class Form extends Component {
    constructor(props){        
